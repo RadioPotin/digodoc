74954f6b3cfd9589eb194edf9178b13ee42038a1
Hernouf Mohamed <mohamed.hernouf@ocamlpro.com>
2021-08-24 12:23:19 +0200

    Search redirection

--- src/frontend/search.ml
+++ src/frontend/search.ml
@@ -14,6 +14,23 @@ open Js_of_ocaml
 open Js
 open Global
 
+let redirection_handler () =
+    let search_items = document##getElementsByClassName (js "search-item") in
+    let _ = 
+        if search_items##.length == 1
+        then begin
+            let item =  unopt @@ search_items##item 0 in
+            let link =  unopt @@ Dom.CoerceTo.element @@ unopt @@ item##.firstChild in
+            let href = unopt @@ link##getAttribute (js "href") in
+            window##open_ href (js "_self") (Opt.return (js ""))
+        end
+        else
+            let url = js ("search.html?pattern=" ^ state.pattern) in 
+            window##open_ url (js "_self") (Opt.return (js "")) 
+    in
+        logs "PES";
+        ()
+
 let clear_search () =
     match !search_ul with 
     | Some search_ul ->
@@ -33,19 +50,33 @@ let update_search () = Lwt.async Requests.sendSearchRequest
 
 let set_search_handler () =
     let search = unopt @@ Html.CoerceTo.input @@ getElementById "search" in
-    search##.onkeyup := Html.handler (fun _ ->
-        clear_search ();
-        let re = search##.value in
-        let input = re##trim in
-        state.pattern <- to_string input;
-        if String.length state.pattern > 2
-        then update_search ();
+    search##.onkeyup := Html.handler (fun kbevent ->
+        begin
+            match Option.map to_string @@ Optdef.to_option @@ kbevent##.key with
+            | Some "Enter" -> 
+                redirection_handler ()
+            | _ -> begin
+                clear_search ();
+                let re = search##.value in
+                let input = re##trim in
+                state.pattern <- to_string input;
+                if String.length state.pattern > 2
+                then update_search ();
+            end
+        end;
+        _false)
+
+let set_button_handler () =
+    let button = unopt @@ Html.CoerceTo.button @@ getElementById "search-button" in
+    button##.onclick := Html.handler (fun _ ->
+        redirection_handler ();
         _false)
 
 let onload_main () =
     let%lwt () = Requests.api_host () in
     clear_search ();
     set_search_handler ();
+    set_button_handler ();
     Headfoot.activate_bar ();
     Headfoot.footerHandler ();
     Lwt.return_unit
