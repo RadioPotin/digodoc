40160008aaa7626cbe81244181c082abfb9865ef
RadioPotin <31403378+RadioPotin@users.noreply.github.com>
2021-09-14 10:35:16 +0000

    Deploying to gh-pages from  @ 274e39aad2926041c4889b71b6f7dcf92ac18c38 🚀

--- sphinx/_static/documentation_options.js
+++ sphinx/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'master (d346f2a) (2021/07/27 16:28)',
+    VERSION: 'master (274e39a) (2021/09/14 10:35)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
